ff6ba2485059e59603782facb82caf4769bc33be
anurag <jhaanurag407@gmail.com>
2024-05-25 14:20:25 +0530

    changes

---
+++ netflix/src/utils/constant.js
@@ -0,0 +1,16 @@
+export const API_END_POINT = "https://netflix2-0-2-b20b.onrender.com/api/v1/user";
+export const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiOTczOTRlMWJlYmI5MjZhNGY5YjgzNTU3ZmQxYTE4YiIsInN1YiI6IjY2MDkxMTQyMmZhZjRkMDE0YWM3MWQ4NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.y3Z8N1gt2AUnrQ4Njemy16PlFWn1UdKpTd1bioMs9yU'
+  }
+};
+export const Now_Playing_Movie = "https://api.themoviedb.org/3/movie/now_playing";
+export const Popular_Movie = "https://api.themoviedb.org/3/movie/popular";
+export const Top_Rated_Movie = "https://api.themoviedb.org/3/movie/top_rated";
+export const Upcoming_Movie = "https://api.themoviedb.org/3/movie/upcoming";
+
+export const  SEARCH_MOVIE_URL="https://api.themoviedb.org/3/search/movie?query=";
+
+export const TMDB_IMG_URL = "https://image.tmdb.org/t/p/w500";
